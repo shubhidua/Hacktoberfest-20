1f54d43e06e06a0e0f51a8e80c59fb92f3ae6848
Abhimanyu Deswal <abhimanyudeswal07@gmail.com>
2021-10-05 13:46:27 +0530

    Binary Exponentiation

---
+++ BinaryExponentiation.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+long long binpow(long long a, long long b) {
+    long long res = 1;
+    while (b > 0) {
+        if (b & 1)
+            res = res * a;
+        a = a * a;
+        b >>= 1;
+    }
+    return res;
+}
+
+int main() {
+    long long a, b;
+    cin >> a >> b;
+    long long p = binpow(a, b);
+    cout << p; 
+}
